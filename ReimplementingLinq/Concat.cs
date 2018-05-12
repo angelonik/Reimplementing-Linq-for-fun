a90a02f911c5a880dc7ba537cad35a90c819eeab
angelo_nikoloulo <angelosnikoloulo@gmail.com>
2018-05-12 23:28:02 +0100

    concat implementation

---
+++ ReimplementingLinq/Concat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReimplementingLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Concat<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            return ConcatImpl(
+                first ?? throw new ArgumentNullException(nameof(first)), 
+                second ?? throw new ArgumentNullException(nameof(second)));
+        }
+
+        private static IEnumerable<TSource> ConcatImpl<TSource>(
+            IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            foreach (TSource item in first)
+            {
+                yield return item;
+            }
+
+            // Avoid hanging onto a reference we don’t really need 
+            first = null;
+            foreach (TSource item in second)
+            {
+                yield return item;
+            }
+        }
+    }
+
+}
