d5d83b9b4ed2c9f52f5b70e2f5417b654e646851
angelo_nikoloulo <angelosnikoloulo@gmail.com>
2018-05-12 21:20:45 +0100

    implemented repeat

---
+++ ReimplementingLinq/Repeat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReimplementingLinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+            return RepeatImpl(element, count);
+        }
+
+        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return element;
+            }
+        }
+    }
+
+}
